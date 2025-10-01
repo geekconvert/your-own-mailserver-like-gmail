5a5fcc75813050f56c2e00d7200025a507eb75ec
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-9.local>
2025-10-01 12:06:03 +0530

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const { SMTPServer } = require("smtp-server");
+const server = new SMTPServer({
+  allowInsecureAuth: true,
+  authOptional: true,
+  onConnect: (session, callback) => {
+    console.log(`onConnect: ${session.id}`);
+    callback();
+  },
+  onMailFrom: (address, session, callback) => {
+    console.log(`onMailFrom: ${address.address} ${session.id}`);
+    callback();
+  },
+  onRcptTo: (address, session, callback) => {
+    console.log(`onRcptTo: ${address.address} ${session.id}`);
+    callback();
+  },
+  onData: (stream, session, callback) => {
+    console.log(`onData: ${session.id}`);
+    stream.on("data", (chunk) => {
+      console.log(`Data chunk: ${chunk.length} bytes`);
+    });
+    stream.on("end", () => {
+      console.log("End of data");
+      callback();
+    });
+  },
+});
+
+server.listen(port, () => console.log("SMTP server is running"));
